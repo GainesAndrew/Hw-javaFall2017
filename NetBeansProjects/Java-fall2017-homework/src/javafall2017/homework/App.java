9b6cab1025b79f98692dfd584e943f5bab127716
GainesAndrew <31869886+GainesAndrew@users.noreply.github.com>
2017-10-30 15:21:47 -0600

    commiting final hm4

--- NetBeansProjects/Java-fall2017-homework/src/javafall2017/homework/App.java
+++ NetBeansProjects/Java-fall2017-homework/src/javafall2017/homework/App.java
@@ -1,31 +1,151 @@
 package javafall2017.homework;
 
-import java.util.Arrays;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * Programer: Andrew Gaines
- * 
- * About: Java fall2017
- *        HomeWork 3 
- *        Dr. Macevoy
- *        10/12/2017
+ *
+ * About: Java fall2017 HomeWork 4 Dr. Macevoy 10/31/2017 // program adds NBA
+ * players name, pointesPerGame, and assistsPerGame to sqlite database 
  */
-
-
 public class App {
-    String [] args;
-    App(String[] _args) {
-        args = _args;
+
+    private Connection connect = null;
+    Statement statement = null;
+    ResultSet result = null;
+    long id = -1;
+    String player;
+
+    // connect to sqlite
+    public Connection connectToDataBase() {
+        if (connect == null) {
+            synchronized (this) {
+                if (connect == null) {
+                    try {
+                        connect = DriverManager.getConnection("jdbc:sqlite:sample.db");
+                    } catch (SQLException ex) {
+                        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        }
+        return connect;
     }
 
-    public static void main(String[] args) throws Exception {
-        App app = new App(args);
-        app.run();
+    void reset() {
+        try {
+            connect = connectToDataBase();
+            statement = connect.createStatement();
+            statement.setQueryTimeout(3);
+
+            statement.executeUpdate("drop table if exists playerStats");
+            statement.executeUpdate("create table playerStats (id integer primary key,"
+                    + " name string, pointsPerGame double, assistsPerGame double)");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    void run() throws Exception {
-        System.out.println("args: " + Arrays.toString(args));
+
+    long insert(String name, double pointsPerGame, double assistsPerGame) {
+        String sql = "INSERT INTO playerStats(name,pointsPerGame, assistsPerGame) VALUES(?,?,?)";
+
+        try {
+            Connection connection = connectToDataBase();
+
+            PreparedStatement ps
+                    = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, name);
+            ps.setDouble(2, pointsPerGame);
+            ps.setDouble(3, assistsPerGame);
+            ps.executeUpdate();
+            ResultSet result = ps.getGeneratedKeys();
+            if (result != null && result.next()) {
+                id = result.getLong(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return id;
     }
-    
+
+    // returns name of player
+    String getPlayer(long id) {
+        try {
+            Connection connect = connectToDataBase();
+            String sql = "select name from playerStats where id=?";
+            PreparedStatement ps = connect.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            ps.setLong(1, id);
+            ResultSet result = ps.executeQuery();
+            if (result != null && result.next()) {
+                return result.getString("name");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    // returns ppg
+    double getPointsPerGame(long id) {
+        try {
+            Connection connect = connectToDataBase();
+            String sql = "select pointsPerGame from playerStats where id=?";
+            PreparedStatement ps = connect.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            ps.setLong(1, id);
+            ResultSet result = ps.executeQuery();
+            if (result != null && result.next()) {
+                return result.getDouble("pointsPerGame");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
+
+    // returns apg
+    double getAssistsPerGame(long id) {
+        try {
+            Connection connect = connectToDataBase();
+            String sql = "select assistsPerGame from playerStats where id=?";
+            PreparedStatement ps = connect.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
+            ps.setLong(1, id);
+            ResultSet result = ps.executeQuery();
+            if (result != null && result.next()) {
+                return result.getDouble("assistsPerGame");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return -1;
+    }
+
+    void run() {
+        reset();
+
+        // adding player name to data base
+        long jStocktonId = insert("John Stockton", 13.1, 10.5);
+        long sCurryName = insert("Steph Curry", 29.2, 6.3);
+
+        // printing out players id with name
+        System.out.println("John Stockton Id = " + jStocktonId);
+        System.out.println("John Stockton name = " + getPlayer(1));
+        System.out.println("John Stockton PointPerGame = " + getPointsPerGame(1));
+        System.out.println("John Stockton AssistsPerGame =" + getAssistsPerGame(1));
+        System.out.println("Steph Curry id = " + sCurryName);
+        System.out.println("Steph Curry name = " + getPlayer(2));
+        System.out.println("Steph Curry PointPerGame = " + getPointsPerGame(2));
+        System.out.println("Steph Curry AssistsPerGame =" + getAssistsPerGame(2));
+
+    }
 }
---
+++ NetBeansProjects/Java-fall2017-homework/src/javafall2017/homework/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafall2017.homework;
+import java.sql.*;
+
+/**
+ *
+ * @author Andrew Gaines
+ * 
+ */
+public class MainClass {
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        app.run();
+    
+}
+}
