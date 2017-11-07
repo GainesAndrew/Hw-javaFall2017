c40ce0a089c71f14257f86a1f2eed9be031f3e40
Andrew Gaines <31869886+GainesAndrew@users.noreply.github.com>
2017-11-07 00:32:43 -0700

    Delete Points.java

--- NetBeansProjects/Java-fall2017-homework/src/javafall2017/homework/Points.java
+++
@@ -1,8 +0,0 @@
-package javafall2017.homework;
-
-// Programer: Andrew Gaines
-
-public interface Points extends Comparable <Points> {
-    public void action();
-    public String description();
-}
